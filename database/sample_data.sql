ad481993da5fae9ca13ce45c4270abb84b3a8112
Jeri <jerdut330@gmail.com>
2026-05-20 21:28:55 +0700

    Update sample_data.sql

--- database/sample_data.sql
+++ database/sample_data.sql
@@ -1,1 +1,7 @@
+INSERT INTO merchant (merchant_name, merchant_email, password_hash, business_type)
+VALUES 
+('Jeri Electronics', 'merchant@example.com', 'hashed_password_example', 'Electronics');
 
+INSERT INTO customer (customer_name, customer_email, customer_pnumber, customer_address)
+VALUES 
+('Andi Wijaya', 'andi@example.com', '08123456789', 'Jakarta, Indonesia');
