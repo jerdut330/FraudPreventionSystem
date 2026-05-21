b3a7528ff92fcf509f670a08cb2bbb7963901eef
Jeri <jerdut330@gmail.com>
2026-05-21 19:43:33 +0700

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,10 @@
+import psycopg2
 
+def get_connection():
+    return psycopg2.connect(
+        host="localhost",
+        database="fraud_prevention_system",
+        user="postgres",
+        password="akusukakamu",
+        port="5432"
+    )
